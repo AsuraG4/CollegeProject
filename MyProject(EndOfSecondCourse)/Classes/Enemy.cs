e37a44da95531480a975d684fa1b9f7922610e8e
uglovkasad1@gmail.com <uglovkasad1@gmail.com>
2022-06-10 13:14:04 +0300

    Доработал интерфейс

---
+++ MyProject(EndOfSecondCourse)/Classes/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProject_EndOfSecondCourse_.Classes
+{
+    internal class Enemy
+    {
+        private string MonsterType;
+
+        private int HeathPoints;
+
+        private int EnemyLevel;
+
+        private int AttackPoints;
+        
+
+        public Enemy(string ImagePath, int Heath, int Level, int Attack)
+        {
+            MonsterType = ImagePath;
+            HeathPoints = Heath;
+            EnemyLevel = Level;
+            AttackPoints = Attack;
+        }
+    }
+}
